0ed42fc9125dd81e5003761b2540a29e000439df
William Bickham <bickhamw@classwork.engr.oregonstate.edu>
2025-02-26 09:41:10 -0800

    refactor: moved endpoints to a new route, modularizing the code for future changes

--- FlipperServer.js
+++ FlipperServer.js
@@ -3,6 +3,9 @@ const app = express();
 const argon2 = require('argon2')
 const cors = require('cors');
 
+// Routes
+const libraryMembersRoutes = require('./routes/libraryMembers')
+
 PORT = 6834
 
 const db = require('./db-connector')
@@ -11,74 +14,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/lm', libraryMembersRoutes)
 
-app.get('/LibraryMembers', (req, res) =>
-    {
-        // Define our queries
-        query1 = 'SELECT * FROM LibraryMembers;';
-
-        db.pool.query(query1, function (err, results, fields){
-            res.send(JSON.stringify(results))
-        });
-    });
-
-async function hash(password) {
-    return await argon2.hash(`${password}`);
-}
-
-app.post('/createLibraryMember', async (req, res) => {
-    const { email, username, name, password } = req.body;
-    const passwordHash = await hash(password);
-    const insertQuery = `INSERT INTO LibraryMembers (email, username, name, passwordHash) 
-                         VALUES (?, ?, ?, ?);`
-
-    db.pool.query(insertQuery, [email, username, name, passwordHash], (error, results) => {
-        if (error) {
-            console.error('Database error:', error);
-            return res.status(500).json({ error: 'Database create failed' });
-        }
-        res.json({ message: 'Library member created successfully' });
-    });
-});
-
-app.put('/updateLibraryMember', async (req, res) => {
-    const { email, username, name, password, libraryMemberID } = req.body;
-    const passwordHash = await hash(password)
-    const updateQuery = `UPDATE LibraryMembers SET 
-    email = ?, 
-    username = ?,
-    name = ?,
-    passwordHash = ?
-    WHERE libraryMemberID = ?
-    `
-    db.pool.query(updateQuery, [email, username, name, passwordHash, libraryMemberID], (error, results) => {
-        if (error) {
-            console.error('Database error:', error);
-            return res.status(500).json({ error: 'Database update failed' });
-        }
-
-        if (results.affectedRows === 0) {
-            return res.status(404).json({ error: 'Library member not found' });
-        }
-
-        res.json({ message: 'Library member updated successfully' });
-    });
-})
-
-app.delete('/deleteLibraryMember', async (req, res) => {
-    const { email } = req.body;
-    const deleteQuery = `DELETE FROM LibraryMembers WHERE email = ?`
-    db.pool.query(deleteQuery, [email], (error, results) => {
-        if (error) {
-            console.log('Database Error: ', error);
-            return res.status(500).json({ error: 'Database Delete Failed'})
-        }
-        if (results.affectedRows ===0) {
-            return res.status(404).json({ error: 'Library member not found. Check your spelling!'})
-        }
-        res.json({ message: 'Library member deleted successfully' });
-    })
-})
 
 app.listen(PORT, () => 
     console.log(`FlipperServer now listening on PORT ${PORT}`)
