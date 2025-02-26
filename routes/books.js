6051ff74a3e14bd893119124ede6608b482f050d
William Bickham <bickhamw@classwork.engr.oregonstate.edu>
2025-02-26 10:14:53 -0800

    feat: create skeleton for CRUD, write Read query

--- routes/books.js
+++ routes/books.js
@@ -4,13 +4,20 @@ const db = require('../db-connector')
 
 
 // SELECT's all books in Books table
-router.get('/Books', (req, res) => {})
+router.get('/Books', (req, res) => {
+    const getAllBooks = 'SELECT * FROM Books;';
+    
+    db.pool.query(getAllBooks, function (err, results, fields){
+        res.send(JSON.stringify(results))});
+})
 
-// INSERT a new book
-router.post('/insertBook', (req, res) => {})
+// // INSERT a new book
+// router.post('/insertBook', (req, res) => {})
 
-// UPDATE a book
-router.put('updateBook', (req, res) => {})
+// // UPDATE a book
+// router.put('/updateBook', (req, res) => {})
 
-// DELETE a book
-router.delete('deleteBook', (req, res) => {})
+// // DELETE a book
+// router.delete('/deleteBook', (req, res) => {})
+
+module.exports = router;
