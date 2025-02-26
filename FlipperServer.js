7be8858bc74d38e5350289249ed81c3f6d4adffe
William Bickham <bickhamw@classwork.engr.oregonstate.edu>
2025-02-26 13:47:55 -0800

    maintenance: Moving code around improving flow

--- FlipperServer.js
+++ FlipperServer.js
@@ -3,17 +3,17 @@ const app = express();
 const cors = require('cors');
 PORT = 6834;
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Routes
 const libraryMembersRoutes = require('./routes/libraryMembers');
 const books = require('./routes/books');
 const memberCheckouts = require('./routes/memberCheckouts');
 const memberFees = require('./routes/memberFees')
 
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+// Pointing to the routes
 app.use('/lm', libraryMembersRoutes);
 app.use('/b', books);
 app.use('/mc', memberCheckouts);
