4929f5ace6f8012a4bd919187433d6ee33ac2b39
William Bickham <bickhamw@classwork.engr.oregonstate.edu>
2025-03-06 13:51:53 -0800

    Feat: completed CRUD functionality for MemberFees entity

--- routes/memberFees.js
+++ routes/memberFees.js
@@ -10,12 +10,69 @@ router.get('/MemberFees', (req, res) => {
 })
 
 // // INSERT a new fee
-// router.post('/insertMemberFee', (req, res) => {})
+router.post('/insertMemberFee', (req, res) => {
+    const { feeAmount, title, email} = req.body
+    const insertQuery = `
+                        INSERT INTO MemberFees (libraryMemberID, memberCheckoutID, feeAmount)
+                        SELECT lm.libraryMemberID, mc.memberCheckoutID, ?
+                        FROM MemberCheckouts AS mc
+                        JOIN LibraryMembers AS lm ON mc.libraryMemberID = lm.libraryMemberID
+                        JOIN Books as b ON mc.bookID = b.bookID
+                        WHERE b.title = ? AND lm.email = ?;
+                        `
+    db.pool.query(insertQuery, [feeAmount, title, email], (error, results) => {
+        if (error) {
+            console.error('Database error:', error);
+            return res.status(500).json({ error: 'MemberFees create failed' });
+        }
+        res.json({ message: `Fee created successfully: ${results}` });
+    })
+})
 
 // // UPDATE a fee
-// router.put('/updateMemberFee', (req, res) => {})
+router.put('/updateMemberFee', (req, res) => {
+    const { email, title, feeAmount, paymentStatus, memberFeeID } = req.body;
+    const updateQuery = `
+                        UPDATE MemberFees SET 
+                        libraryMemberID = (SELECT libraryMemberID FROM LibraryMembers WHERE email = ?),
+                        memberCheckoutID = CASE 
+                        WHEN ? = '' OR ? IS NULL THEN NULL
+                        ELSE (
+                            SELECT mc.memberCheckoutID FROM MemberCheckouts as mc
+                            JOIN LibraryMembers AS lm ON mc.libraryMemberID = lm.libraryMemberID
+                            JOIN Books AS b ON mc.bookID = b.bookID
+                            WHERE lm.email = ? AND (b.title = ? OR ? IS NULL)
+                            ORDER BY mc.checkoutDate DESC
+                            LIMIT 1
+                        )
+                        END,
+                        feeAmount = ?,
+                        paymentStatus = ?
+                        WHERE memberFeeID = ?;
+                        `
+    db.pool.query(updateQuery, [email, title, title, email, title, title, feeAmount, paymentStatus, memberFeeID], (error, results) => {
+        if (error) {
+            console.error('Database error:', error);
+            return res.status(500).json({ error: 'MemberCheckouts create failed' });
+        }
+        res.json({ message: `Member Fee updated successfully: ${results}` });
+    });
+});
 
 // // DELETE a fee
-// router.delete('/deleteMemberFee', (req, res) => {})
+router.delete('/deleteMemberFee', (req, res) => {
+    const { memberFeeID } = req.body;
+    const deleteQuery = `DELETE FROM MemberFees WHERE  memberFeeID = ?;`
+    db.pool.query(deleteQuery, [memberFeeID], (error, results) => {
+        if (error) {
+            console.log('Database Error: ', error);
+            return res.status(500).json({ error: 'MemberFees Delete Failed'})
+        }
+        if (results.affectedRows ===0) {
+            return res.status(404).json({ error: 'Fee not found. Check your spelling!'})
+        }
+        res.json({ message: `Fee deleted successfully: ${results}` });
+    });
+})
 
 module.exports = router;
