7a336a65e11624279493da259e980f22abeb0f41
William Bickham <bickhamw@classwork.engr.oregonstate.edu>
2025-03-06 22:06:29 -0800

    fix: json return was jumbled in order, added order by asc

--- routes/dropDown.js
+++ routes/dropDown.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../db-connector');
 
 router.get('/emails', (req, res) => {
-    const readAllEmailsQuery = 'SELECT libraryMemberID, email FROM LibraryMembers;'
+    const readAllEmailsQuery = 'SELECT libraryMemberID, email FROM LibraryMembers ORDER BY libraryMemberID ASC;'
     db.pool.query(readAllEmailsQuery, (error, results) => {
         res.send(JSON.stringify(results))
     })
@@ -11,7 +11,7 @@ router.get('/emails', (req, res) => {
 
 
 router.get('/titles', (req, res) => {
-    const readAllTitlesQuery = 'SELECT bookID, title FROM Books;'
+    const readAllTitlesQuery = 'SELECT bookID, title FROM Books ORDER BY bookID ASC;'
     db.pool.query(readAllTitlesQuery, (error, results) => {
         res.send(JSON.stringify(results))
     })
