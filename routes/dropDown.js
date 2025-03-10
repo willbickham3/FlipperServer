4e6ea145be54c057634d14dd4f34822192f9052f
William Bickham <bickhamw@classwork.engr.oregonstate.edu>
2025-03-09 23:57:46 -0700

    feat: add comments for different routes

--- routes/dropDown.js
+++ routes/dropDown.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db-connector');
 
+// SELECTS libraryMemberID and Emails for all library members for drop down list
 router.get('/emails', (req, res) => {
     const readAllEmailsQuery = 'SELECT libraryMemberID, email FROM LibraryMembers ORDER BY libraryMemberID ASC;'
     db.pool.query(readAllEmailsQuery, (error, results) => {
@@ -9,7 +10,7 @@ router.get('/emails', (req, res) => {
     })
 })
 
-
+// SELECTS bookID's and Titles from books for drop down list
 router.get('/titles', (req, res) => {
     const readAllTitlesQuery = 'SELECT bookID, title FROM Books ORDER BY bookID ASC;'
     db.pool.query(readAllTitlesQuery, (error, results) => {
@@ -17,6 +18,8 @@ router.get('/titles', (req, res) => {
     })
 })
 
+
+// SELECT email/title combinations for MemberCheckouts Entries for Drop Down
 router.get('/checkouts', (req, res) => {
     const readAllCheckouts = `
                             SELECT lm.email, b.title FROM
