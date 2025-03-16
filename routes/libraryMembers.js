6678beac57d7c921ef7c6f4624899f2ea2c351a5
William Bickham <bickhamw@classwork.engr.oregonstate.edu>
2025-03-15 23:14:33 -0700

    feat: add validation to libraryMembers CRUD

--- routes/libraryMembers.js
+++ routes/libraryMembers.js
@@ -21,14 +21,22 @@ router.get('/LibraryMembers', (req, res) =>
 // CREATE/INSERT a library member
 router.post('/insertLibraryMember', async (req, res) => {
     const { email, username, name, password } = req.body;
-    const passwordHash = await hash(password);
-    const insertQuery = `INSERT INTO LibraryMembers (email, username, name, passwordHash) 
-                         VALUES (?, ?, ?, ?);`
 
-    db.pool.query(insertQuery, [email, username, name, passwordHash], (error, results) => {
-        if (error) {
-            console.error('Database error:', error);
-            return res.status(500).json({ error: 'Database create failed' });
+    if (!email || !username || !name || !password) {
+        return res.status(400).json({ 'Error': 'Email, username, name, or password missing/incorrect format.' })
+    }
+
+    const passwordHash = await hash(password);
+    const insertQuery = `
+                        INSERT INTO LibraryMembers 
+                        (email, username, name, passwordHash) 
+                        VALUES (?, ?, ?, ?);
+                        `
+
+    db.pool.query(insertQuery, [email, username, name, passwordHash], (err, results) => {
+        if (err) {
+            console.error('Database error:', err);
+            return res.status(500).json({ err: 'Database create failed' });
         }
         res.json({ message: 'Library member created successfully' });
     });
@@ -37,20 +45,22 @@ router.post('/insertLibraryMember', async (req, res) => {
 // UPDATE a library member
 router.put('/updateLibraryMember', async (req, res) => {
     const { email, username, name, libraryMemberID } = req.body;
-    const updateQuery = `UPDATE LibraryMembers SET 
-    email = ?, 
-    username = ?,
-    name = ?
-    WHERE libraryMemberID = ?;
-    `
-    db.pool.query(updateQuery, [email, username, name, libraryMemberID], (error, results) => {
-        if (error) {
-            console.error('Database error:', error);
-            return res.status(500).json({ error: 'Database update failed' });
-        }
 
-        if (results.affectedRows === 0) {
-            return res.status(404).json({ error: 'Library member not found' });
+    if (!email || !username || !name || !password) {
+        return res.status(400).json({ 'Error': 'Email, username, name, or password missing/incorrect format.' })
+    }
+
+    const updateQuery = `
+                        UPDATE LibraryMembers SET 
+                        email = ?, 
+                        username = ?,
+                        name = ?
+                        WHERE libraryMemberID = ?;
+                        `
+    db.pool.query(updateQuery, [email, username, name, libraryMemberID], (err, results) => {
+        if (err) {
+            console.error('Database error:', err);
+            return res.status(500).json({ err: 'Database update failed' });
         }
 
         res.json({ message: 'Library member updated successfully' });
@@ -61,14 +71,13 @@ router.put('/updateLibraryMember', async (req, res) => {
 router.delete('/deleteLibraryMember', async (req, res) => {
     const { email } = req.body;
     const deleteQuery = `DELETE FROM LibraryMembers WHERE email = ?;`
-    db.pool.query(deleteQuery, [email], (error, results) => {
-        if (error) {
-            console.log('Database Error: ', error);
-            return res.status(500).json({ error: 'Database Delete Failed'})
+    
+    db.pool.query(deleteQuery, [email], (err, results) => {
+        if (err) {
+            console.log('Database Error: ', err);
+            return res.status(500).json({ err: 'Database Delete Failed'})
         }
-        if (results.affectedRows ===0) {
-            return res.status(404).json({ error: 'Library member not found. Check your spelling!'})
-        }
+        
         res.json({ message: 'Library member deleted successfully' });
     })
 })
