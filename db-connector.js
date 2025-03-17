b5fcd0e4b89bbcac04477bd36d495b6fc038f50f
William Bickham <bickhamw@classwork.engr.oregonstate.edu>
2025-03-16 20:46:56 -0700

    maitenance: add citation for db-connector starter

--- db-connector.js
+++ db-connector.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 const mysql = require('mysql')
 
+// Citation for the db connector:
+// Date : 02/23/2025
+// Based on:
+// Source URL: https://canvas.oregonstate.edu/courses/1987790/assignments/9888486?module_item_id=25022943
+
+// Provide credentials for connecting to database
 const pool = mysql.createPool({
     connectionLimit : process.env.DB_CONNECTION_LIMIT,
     host            : process.env.DB_HOST,
